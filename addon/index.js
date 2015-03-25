b2c6b927b97b3a7b85049557d24da8bda7901f5e
Sam Selikoff <sam.selikoff@gmail.com>
2015-03-25 15:47:08 -0400

    refactoring

--- addon/index.js
+++ addon/index.js
@@ -1,7 +1,15 @@
 import Factory from './factory';
 import Response from './response';
+import Sequence from './attributes/sequence';
+import Lazy from './attributes/lazy';
 
 export default {
   Factory: Factory,
   Response: Response,
+  sequence: function(def) {
+    return new Sequence(def);
+  },
+  lazy: function(def) {
+    return new Lazy(def);
+  }
 };
