c9c7cb741e21d2ef23c62d541d3bfcfe0f195f4f
Sam Selikoff <sam.selikoff@gmail.com>
2015-04-19 16:47:10 -0400

    working on belongs to

--- tests/integration/schema/belongs-to-test.js
+++ tests/integration/schema/belongs-to-test.js
@@ -4,24 +4,60 @@ import Schema from 'ember-cli-mirage/orm/schema';
 import Db from 'ember-cli-mirage/orm/db';
 import {module, test} from 'qunit';
 
-var db, schema, User, Address;
-module('mirage:integration:schema:belongsTo', {
+var schema;
+module('mirage:integration:schema:belongsTo#create', {
   beforeEach: function() {
-    db = new Db();
-    db.createCollection('users');
-    db.users.insert([
-      {id: 1, name: 'Link'},
-      {id: 2, name: 'Zelda'}
-    ]);
-    db.createCollection('addresses');
-    db.addresses.insert([
-      {id: 1, user_id: 1, name: '123 Hyrule Way'},
-      {id: 2, name: '456 Goron City'}
-    ]);
+    var db = new Db();
+    db.loadData({
+      users: [],
+      addresses: [
+        {id: 1, name: '123 Hyrule Way'}
+      ]
+    });
     schema = new Schema(db);
 
-    User = Model.extend();
-    Address = Model.extend({
+    var User = Model.extend();
+    var Address = Model.extend({
+      user: Mirage.belongsTo()
+    });
+
+    schema.register('user', User);
+    schema.register('address', Address);
+  }
+});
+
+test('the child can create its parent model', function(assert) {
+  var address = schema.address.find(1);
+
+  assert.equal(address.user, null);
+  assert.equal(schema.user.all().length, 0);
+
+  var ganon = address.createUser({name: 'Ganon'});
+
+  assert.ok(ganon.id);
+  assert.deepEqual(ganon, schema.user.find(ganon.id));
+  assert.equal(address.user_id, ganon.id);
+});
+
+
+var schema;
+module('mirage:integration:schema:belongsTo#read', {
+  beforeEach: function() {
+    var db = new Db();
+    db.loadData({
+      users: [
+        {id: 1, name: 'Link'},
+        {id: 2, name: 'Zelda'}
+      ],
+      addresses: [
+        {id: 1, user_id: 1, name: '123 Hyrule Way'},
+        {id: 2, name: '456 Goron City'}
+      ]
+    });
+    schema = new Schema(db);
+
+    var User = Model.extend();
+    var Address = Model.extend({
       user: Mirage.belongsTo()
     });
 
@@ -43,6 +79,33 @@ test('it returns null if no parent model is found', function(assert) {
   assert.deepEqual(address.user, null);
 });
 
+
+var schema;
+module('mirage:integration:schema:belongsTo#update', {
+  beforeEach: function() {
+    var db = new Db();
+    db.createCollection('users');
+    db.users.insert([
+      {id: 1, name: 'Link'},
+      {id: 2, name: 'Zelda'}
+    ]);
+    db.createCollection('addresses');
+    db.addresses.insert([
+      {id: 1, user_id: 1, name: '123 Hyrule Way'},
+      {id: 2, name: '456 Goron City'}
+    ]);
+    schema = new Schema(db);
+
+    var User = Model.extend();
+    var Address = Model.extend({
+      user: Mirage.belongsTo()
+    });
+
+    schema.register('user', User);
+    schema.register('address', Address);
+  }
+});
+
 test('the child can update the parent model', function(assert) {
   var address = schema.address.find(1);
   var link = schema.user.find(1);
@@ -53,13 +116,4 @@ test('the child can update the parent model', function(assert) {
   address.user = zelda;
 
   assert.deepEqual(address.user, zelda);
-});
-
-test('the child can create its parent model', function(assert) {
-  var address = schema.address.find(1);
-  var ganon = address.createUser({name: 'Ganon'});
-
-  assert.ok(ganon.id);
-  assert.deepEqual(ganon, schema.user.find(ganon.id));
-  assert.equal(address.user_id, ganon.id);
 });
